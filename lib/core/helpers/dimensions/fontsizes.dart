28a2efb16920ee8e02b8b5e1441f8fb06d71cbb1
Anderson-Andre-P <eleu.ander@gmail.com>
2023-11-01 08:47:42 -0300

    feat: add dimensions

---
+++ lib/core/helpers/dimensions/fontsizes.dart
@@ -0,0 +1,14 @@
+class AppFontSizes {
+  double get fs10 => 10;
+  double get fs12 => 12;
+  double get fs13 => 13;
+  double get fs14 => 14;
+  double get fs15 => 15;
+  double get fs16 => 16;
+  double get fs19 => 19;
+  double get fs23 => 23;
+  double get fs33 => 33;
+  double get fs46 => 46;
+  double get fs58 => 58;
+  double get fs93 => 93;
+}
---
+++ lib/core/helpers/dimensions/sizes.dart
@@ -0,0 +1,13 @@
+class AppSizes {
+  double get s2 => 2;
+  double get s4 => 4;
+  double get s8 => 8;
+  double get s12 => 12;
+  double get s16 => 16;
+  double get s24 => 24;
+  double get s32 => 32;
+  double get s48 => 48;
+  double get s64 => 64;
+  double get s96 => 96;
+  double get s124 => 124;
+}
