a9a7bc70684c282d54d9b7091c9482014bb163d2
Anderson-Andre-P <eleu.ander@gmail.com>
2023-11-01 08:55:10 -0300

    feat: create class R contains helpers

---
+++ lib/core/helpers/resources.dart
@@ -0,0 +1,11 @@
+import 'colors/colors.dart';
+import 'dimensions/fontsizes.dart';
+import 'fonts/fonts_family.dart';
+import 'i18n/i18n.dart';
+
+class R {
+  static Translation string = PtBr();
+  static FontsFamily fontFamily = FontsFamily();
+  static AppColors colors = AppColors();
+  static AppFontSizes fontSize = AppFontSizes();
+}
