4c9fd4fd5104da9d7ed0023119a6c103a82a99c3
Anderson-Andre-P <eleu.ander@gmail.com>
2023-11-01 08:46:45 -0300

    feat: add colors

---
+++ lib/core/helpers/colors/colors.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  Color get redError => const Color(0xFFEF4352);
+  Color get yellowAlert => const Color(0xFFFFBA08);
+  Color get greenSuccess => const Color(0xFF118708);
+  Color get blueInfo => const Color(0xFF4393EF);
+
+// Tema Light
+  Color get lightPrimaryColor =>
+      const Color(0xFF2563EB); // Cor primária para o tema light
+  Color get lightSecondaryColor =>
+      const Color(0xFFFFBA08); // Cor secundária para o tema light
+  Color get lightTertiaryColor =>
+      const Color(0xFF118708); // Cor terciária para o tema light
+  Color get lightTitleTextColor =>
+      const Color(0xFF0F1419); // Cor do texto de títulos para o tema light
+  Color get lightCommonTextColor =>
+      const Color(0xFF6C6C6C); // Cor do texto comum para o tema light
+  Color get lightPrimaryButtonColor =>
+      const Color(0xFF2563EB); // Cor do botão primário para o tema light
+  Color get lightSecondaryButtonColor =>
+      const Color(0xFFffffff); // Cor do botão secundário para o tema light
+  Color get lightTertiaryButtonColor =>
+      const Color(0xFFFFBA08); // Cor do botão terciário para o tema light
+  Color get lightPrimaryBackgroundColor =>
+      const Color(0xFFffffff); // Cor de fundo principal para o tema light
+  Color get lightSecondaryBackgroundColor =>
+      const Color(0xFFF1F3F9); // Cor de fundo secundária para o tema light
+  Color get lightTertiaryBackgroundColor =>
+      const Color(0xFFF7F7F7); // Cor de fundo terciária para o tema light
+  Color get lightActiveInputColor =>
+      const Color(0xFFE1E6EF); // Cor do input ativo para o tema light
+  Color get lightDisabledInputColor =>
+      const Color(0xFFF1F3F9); // Cor do input desativado para o tema light
+  Color get lightInputTextColor =>
+      const Color(0xFF6C6C6C); // Cor do texto no input para o tema light
+  Color get lightInputTextHighlightColor => const Color(
+      0xFF6C6C6C); // Cor do texto digitado no input para o tema light
+  Color get lightBorderColor =>
+      const Color(0xFFBEBEBE); // Cor da borda para o tema light
+  Color get lightIconColor =>
+      const Color(0xFF536471); // Cor do ícone para o tema light
+
+// Tema Dark
+  Color get darkPrimaryColor =>
+      const Color(0xFF2563EB); // Cor primária para o tema dark
+  Color get darkSecondaryColor =>
+      const Color(0xFFFFBA08); // Cor secundária para o tema dark
+  Color get darkTertiaryColor =>
+      const Color(0xFF118708); // Cor terciária para o tema dark
+  Color get darkTitleTextColor =>
+      const Color(0xFFF7F9F9); // Cor do texto de títulos para o tema dark
+  Color get darkCommonTextColor =>
+      const Color(0xFF8B98A5); // Cor do texto comum para o tema dark
+  Color get darkPrimaryButtonColor =>
+      const Color(0xFF2563EB); // Cor do botão primário para o tema dark
+  Color get darkSecondaryButtonColor =>
+      const Color(0xFF15202B); // Cor do botão secundário para o tema dark
+  Color get darkTertiaryButtonColor =>
+      const Color(0xFFFFBA08); // Cor do botão terciário para o tema dark
+  Color get darkPrimaryBackgroundColor =>
+      const Color(0xFF15202B); // Cor de fundo principal para o tema dark
+  Color get darkSecondaryBackgroundColor =>
+      const Color(0xFF252E38); // Cor de fundo secundária para o tema dark
+  Color get darkTertiaryBackgroundColor =>
+      const Color(0xFF38444D); // Cor de fundo terciária para o tema dark
+  Color get darkActiveInputColor =>
+      const Color(0xFF273340); // Cor do input ativo para o tema dark
+  Color get darkDisabledInputColor =>
+      const Color(0xFF8B98A5); // Cor do input desativado para o tema dark
+  Color get darkInputTextColor =>
+      const Color(0xFF8B98A5); // Cor do texto no input para o tema dark
+  Color get darkInputTextHighlightColor => const Color(
+      0xFFF7F9F9); // Cor do texto digitado no input para o tema dark
+  Color get darkBorderColor =>
+      const Color(0xFF38444D); // Cor da borda para o tema dark
+  Color get darkIconColor =>
+      const Color(0xFF8B98A5); // Cor do ícone para o tema dark
+}
