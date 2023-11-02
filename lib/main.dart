44d755de8b391cf6488ae80ef93c00cf37c5b290
Anderson-Andre-P <eleu.ander@gmail.com>
2023-11-02 08:35:15 -0300

    feat: showing onboarding only once

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vibraguard/viewmodel/assets_view_model.dart';
+import 'package:vibraguard/views/screens/home_screen.dart';
 import 'package:vibraguard/views/screens/onboarding_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final bool isFirstOpen = prefs.getBool('firstOpen') ?? true;
+
   runApp(
     MultiProvider(
       providers: [
@@ -23,7 +29,9 @@ void main() {
         //   create: (_) => WorkOrdersViewModel(),
         // ),
       ],
-      child: const MainApp(),
+      child: MaterialApp(
+        home: isFirstOpen ? const OnboardingScreen() : const MainApp(),
+      ),
     ),
   );
 }
@@ -33,8 +41,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: OnboardingScreen(),
-    );
+    return const HomeScreen(id: 1);
   }
 }
