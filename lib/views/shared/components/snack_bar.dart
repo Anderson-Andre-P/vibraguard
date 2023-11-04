1bb0a27cf6388646f400afe62aeec1caddb28720
Anderson-Andre-P <eleu.ander@gmail.com>
2023-11-04 01:14:07 -0300

    feat: snack bar

---
+++ lib/views/shared/components/snack_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../core/helpers/resources.dart';
+
+class CustomSnackBar {
+  static showCustomSnackBar(
+      {required String title, required String message, Duration? duration}) {
+    Get.snackbar(
+      title,
+      message,
+      duration: duration ?? const Duration(seconds: 3),
+      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+      colorText: Colors.white,
+      backgroundColor: R.colors.greenSuccess,
+      icon: const Icon(
+        Icons.check_circle,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  static showCustomErrorSnackBar(
+      {required String title,
+      required String message,
+      Color? color,
+      Duration? duration}) {
+    Get.snackbar(
+      title,
+      message,
+      duration: duration ?? const Duration(seconds: 3),
+      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+      colorText: Colors.white,
+      backgroundColor: color ?? R.colors.redError,
+      icon: const Icon(
+        Icons.error,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  static showCustomToast(
+      {String? title,
+      required String message,
+      Color? color,
+      Duration? duration}) {
+    Get.rawSnackbar(
+      title: title,
+      duration: duration ?? const Duration(seconds: 3),
+      snackStyle: SnackStyle.GROUNDED,
+      backgroundColor: color ?? R.colors.greenSuccess,
+      onTap: (snack) {
+        Get.closeAllSnackbars();
+      },
+      //overlayBlur: 0.8,
+      message: message,
+    );
+  }
+
+  static showCustomErrorToast(
+      {String? title,
+      required String message,
+      Color? color,
+      Duration? duration}) {
+    Get.rawSnackbar(
+      title: title,
+      duration: duration ?? const Duration(seconds: 3),
+      snackStyle: SnackStyle.GROUNDED,
+      backgroundColor: color ?? R.colors.redError,
+      onTap: (snack) {
+        Get.closeAllSnackbars();
+      },
+      //overlayBlur: 0.8,
+      message: message,
+    );
+  }
+}
